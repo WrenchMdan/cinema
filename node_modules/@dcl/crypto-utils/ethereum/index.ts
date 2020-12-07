6f8d445e19402cc78a68b2e80b7f14a3bd42def9
WrenchMdan <68812803+WrenchMdan@users.noreply.github.com>
2020-12-07 23:44:27 +0800

    Add files via upload

---
+++ node_modules/@dcl/crypto-utils/ethereum/index.ts
@@ -0,0 +1,14 @@
+import { getUserAccount } from '@decentraland/EthereumController'
+import * as eth from '@decentraland/EthereumController'
+
+/**
+ * Sign a message with your address
+ * 
+ * @param messageToSign String to sign
+ * @returns Initial message and its signature
+ */
+export async function signMessage(messageToSign: string) {
+  const convertedMessage = await eth.convertMessageToObject(messageToSign)
+  const { message, signature } = await eth.signMessage(convertedMessage)
+  return { message, signature }
+}
